419de1f81c6cefa0e5d396d62ba824c24de9cc16
Jason <jasongoesplaces@gmail.com>
2018-09-29 23:07:03 -0400

    home page beginings

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,15 +4,8 @@ import React from "react";
 const Nav = () => (
   <div>
     <nav>
-      <div className="nav-wrapper navBar">
-        <a href="/dashboard" className="brand-logo center"></a>
-        <ul id="nav-mobile" className="right hide-on-med-and-down">
-          <li><a href="/dashboard">Dashboard</a></li>
-          <li><a href="/guides">Guides</a></li>
-          <li><a href="/events">Events</a></li>
-          {/* <li><a href="/forum">Forum</a></li> */}
-          <li><a className="waves-effect waves-light btn" href="/logout">Log Out</a></li>
-        </ul>
+      <div className="nav-wrapper navBar logo">
+        <a href="/" className="brand-logo"><img className="logo" src="./images/NATIVE2.png"/></a>
         <ul id="nav-mobile" className="left hide-on-large-only">
           <li><a className="dropdown-trigger" href="#!" data-target="dropdown1"><i className="material-icons right">menu</i></a></li>
         </ul>
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,19 @@
 import React, { Component } from "react";
 import NavBar from "../../components/Nav"
+import NavBar2 from "../../components/Nav2"
+import {Carousel} from 'react-materialize'
 
 class Home extends Component {
   render() {
     return (
       <div className="landerPage">
       <NavBar />
+      <NavBar2 />
+      <Carousel options={{ fullWidth: true }} images={[
+        './images/placeholder1.jpeg',
+        './images/placeholder2.jpg',
+        './images/placeholder3.jpg',
+      ]} />
       </div>
     );
   }
