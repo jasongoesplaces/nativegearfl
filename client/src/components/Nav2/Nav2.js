13a7645a0f1918335bb9b7ad97bcf10c23dd9a54
Jason <jasongoesplaces@gmail.com>
2018-09-30 21:26:39 -0400

    nav update

--- client/src/components/Nav2/Nav2.js
+++ client/src/components/Nav2/Nav2.js
@@ -5,10 +5,10 @@ const Nav2 = () => (
   <div>
       <div className="nav-wrapper navBar nav2">
         <ul id="nav-mobile" className="center logo">
-          <li><a href="#">Shop</a></li>
-          <li><a href="#">About Us</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Shop </a></li>
+          <li><a href="#">About Us </a></li>
+          <li><a href="#">Contact Us </a></li>
+          <li><a href="#">FAQ </a></li>
         </ul>
       </div>
   </div>
