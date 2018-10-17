ecf795313b749f60f7bb86c01a22d6487c2c2e86
jasongoesplaces <jasongoesplaces@gmail.com>
2018-10-17 16:47:28 -0400

    admin login finalized

--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -2,37 +2,36 @@ import React from "react";
 
 const LoginView = ({ onSubmit }) => {
   return (
-    <div className="landerPage">
-      <div className="landerCard">
-        <div className="container landerContainer">
-          <div className="landerContent">
-              <div className="loginDiv"></div>
-              <form className="col s12 loginField" onSubmit={onSubmit}>
-                  <div className="row">
-                      <div className="col l3 hide-on-med-and-down"></div>
-                      <div className="input-field col s12 l6"> 
-                          <input id="email" type="email" name="email" className="validate inputBar"/>
-                          <label htmlFor="email">Username</label>
-                      </div>
-                      <div className="col l3 hide-on-med-and-down"></div>
-                  </div>
-                  <div className="row">
-                      <div className="col l3 hide-on-med-and-down"></div>
-                      <div className="input-field col s12 l6"> 
-                          <input id="password" type="password" name="password" className="validate inputBar"/>
-                          <label htmlFor="password">Password</label>
-                      </div>
-                      <div className="col l3 hide-on-med-and-down"></div>
-                  </div>
-                  <div>
-                      <button className="button loginSignupBtn">LOGIN</button>
-                  </div>
-              </form>
-            <a className="loginSignupLink" href="/signup">Don't have an account? Signup</a>
-          </div>
+    <div className="adminLogin">
+        <div className="adminLogo">
+            <img src="./images/NATIVE2.png"/>
         </div>
-      </div>
+        <div className="adminLoginText">
+            <h4>Administrator Login</h4>
+        </div>
+        <form className="col s12 loginField" onSubmit={onSubmit}>
+            <div className="row">
+                <div className="col l3 hide-on-med-and-down"></div>
+                <div className="input-field col s12 l6"> 
+                    <input id="email" type="email" name="email" className="validate inputBar"/>
+                    <label htmlFor="email">Username</label>
+                </div>
+                <div className="col l3 hide-on-med-and-down"></div>
+            </div>
+            <div className="row">
+                <div className="col l3 hide-on-med-and-down"></div>
+                <div className="input-field col s12 l6"> 
+                    <input id="password" type="password" name="password" className="validate inputBar"/>
+                    <label htmlFor="password">Password</label>
+                </div>
+                <div className="col l3 hide-on-med-and-down"></div>
+            </div>
+            <div className="buttonContain">
+                <button className="button loginSignupBtn">LOGIN</button>
+            </div>
+        </form>
     </div>
+        
   );
 }
 
