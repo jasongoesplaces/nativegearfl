12f95a40cefd0dc9437d0424e7d77f355a6779a5
jasongoesplaces <jasongoesplaces@gmail.com>
2018-10-18 16:59:11 -0400

    admin authentication added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
+import PrivateRoute from "./components/PrivateRoute";
+import app from "./components/base";
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -9,11 +12,37 @@ import Privacy from "./pages/Privacy";
 import Returns from "./pages/Returns";
 import Terms from "./pages/Terms";
 import Admin from "./pages/Admin";
-// import SignUp from "./pages/Signup";
-// import Logout from "./components/logout"
+import Dashboard from "./pages/Dashboard";
+import Logout from "./components/logout";
 
 class App extends Component {
+  state = { loading: true, authenticated: false, user: null };
+
+  componentWillMount() {
+    app.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          currentUser: user,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          currentUser: null,
+          loading: false
+        });
+      }
+    });
+  }
+
   render() {
+    const { authenticated, loading } = this.state;
+
+    if (loading) {
+      return <p>Loading..</p>;
+    }
+
     return (
       <Router>
         <div>
@@ -24,11 +53,14 @@ class App extends Component {
           <Route exact path="/privacy" component={Privacy}/>
           <Route exact path="/returns" component={Returns}/>
           <Route exact path="/terms" component={Terms}/>
+          <Route exact path="/logout" component={Logout} /> 
           <Route exact path="/admin" component={Admin}/>
-          {/* 
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-          <Route exact path="/logout" component={Logout} /> */}
+          <PrivateRoute
+            exact
+            path="/dashboard"
+            component={Dashboard}
+            authenticated={authenticated}
+          />
         </div>
       </Router>
     );
--- client/src/pages/Admin/Index.js
+++ client/src/pages/Admin/Index.js
@@ -1,1 +1,25 @@
-export { default } from "./Admin.js";
+import React, { Component } from "react";
+import Admin from "./Admin";
+import { withRouter } from "react-router";
+import app from "../../components/base";
+
+class AdminLogin extends Component {
+  handleSignUp = async event => {
+    event.preventDefault();
+    const { email, password } = event.target.elements;
+    try {
+      const user = await app
+        .auth()
+        .signInWithEmailAndPassword(email.value, password.value);
+      this.props.history.push("/dashboard");
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  render() {
+    return <Admin onSubmit={this.handleSignUp} />;
+  }
+}
+
+export default withRouter(AdminLogin);
